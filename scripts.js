7a8dc6d1eda710b8ab8d786083817942c108d587
mathgoms02 <math.gomsx3@gmail.com>
2026-05-14 23:39:43 -0300

    chore: added html components in javascript file

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// Pegando botões pelos atributos
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operator]");
+const equalsButtons = document.querySelector("[data-equals]");
+const deleteButtons = document.querySelector("[data-delete]");
+const previousOperand = document.querySelector("[data-previous-operand]");
+const currentOperand = document.querySelector("[data-current-operand]");
